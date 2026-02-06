0105e5b7871c5de89758434b86781b4abf59e846
mkt-santagroup <marketing@santagroup.gg>
2026-02-06 03:55:30 -0300

    att

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "standalone", // <--- O SEGREDO DA ECONOMIA DE RAM
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**", // Permite imagens de qualquer lugar (Supabase, etc)
+      },
+    ],
+    // Se quiser economizar CPU ao máximo, descomente a linha abaixo (mas perde otimização de imagem)
+    // unoptimized: true, 
+  },
 };
 
 export default nextConfig;
